97988f238fda46b36fa56915f10516cd1340eb40
Juan Lopes <juanplopes@gmail.com>
2011-01-23 23:09:42 -0200

    all functionallity

---
+++ src/MSBuildRunner.Net35/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MSBuildRunner")]
+[assembly: AssemblyDescription("MSBuild runner for .Net 3.5")]
+[assembly: Guid("374251ab-712a-4355-ab7d-a8d832f0ed4d")]
---
+++ src/MSBuildRunner.Net40/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Utilities;
+using System.Diagnostics;
+
+namespace MSBuildRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var msbuild = ToolLocationHelper.GetPathToDotNetFrameworkFile("msbuild.exe", TargetDotNetFrameworkVersion.VersionLatest);
+            var newArgs = string.Join(" ", args.Select(x => "\"" + x + "\"").ToArray());
+            Console.WriteLine("{0} {1}", msbuild, newArgs);
+            var psi = new ProcessStartInfo(msbuild, newArgs);
+            psi.UseShellExecute = false;
+            var p = Process.Start(psi);
+            p.WaitForExit();
+        }
+    }
+}
