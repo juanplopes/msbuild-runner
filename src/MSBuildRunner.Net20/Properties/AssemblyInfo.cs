a69e84afc3ddcff4702a0ce4f9a7fb0f0e23d0da
Juan Lopes <me@juanlopes.net>
2011-06-07 01:03:02 -0300

    adding support to .net 2.0

---
+++ src/MSBuildRunner.Net20/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MSBuildRunner")]
+[assembly: AssemblyDescription("MSBuild runner for .Net 2.0")]
+[assembly: Guid("de852730-abaa-45a4-9bb9-bf593d496e46")]
--- src/MSBuildRunner.Net40/Program.cs
+++ src/MSBuildRunner.Net40/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using Microsoft.Build.Utilities;
 using System.Diagnostics;
@@ -12,7 +11,11 @@ namespace MSBuildRunner
         static int Main(string[] args)
         {
             var msbuild = ToolLocationHelper.GetPathToDotNetFrameworkFile("msbuild.exe", TargetDotNetFrameworkVersion.VersionLatest);
-            var newArgs = string.Join(" ", args.Select(x => "\"" + x + "\"").ToArray());
+
+            for (int i = 0; i < args.Length; i++)
+                args[i] = "\"" + args[i] + "\"";
+
+            var newArgs = string.Join(" ", args);
             Console.WriteLine("{0} {1}", msbuild, newArgs);
             var psi = new ProcessStartInfo(msbuild, newArgs);
             psi.UseShellExecute = false;
